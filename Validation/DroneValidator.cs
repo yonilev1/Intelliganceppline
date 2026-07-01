73958a1d63e79cce2e7185feba4534cf614dd663
Yoni <yonilev2021@gmail.com>
2026-07-01 09:29:57 +0300

    complete dron validation

--- Validation/DroneValidator.cs
+++ Validation/DroneValidator.cs
@@ -2,10 +2,28 @@ using IntelligencePipeline.Models.Reports;
 using IntelligencePipeline.Validation;
 using System.ComponentModel.DataAnnotations;
 
-class DroneValidator: IValidator
+class DroneValidator: BaseValidator
 {
-    public ValidationResult Validate(Report report)
+    protected override ValidationResult ValidateSpecificFields(Report report)
     {
-        return report.GetSourceType() == "Drone" && report.Altitude >= 100 && report.Altitude <= 10000 && report.ImageQuality >= 1 && report.ImageQuality <= 100;
+        ValidationResult commonValid = Validate(report);
+        if (commonValid.IsValid)
+        {
+            if (report.GetSourceType() == "Drone")
+            {
+                DroneReport dr = (DroneReport)report;
+                if (dr.Altitude >= 100 && dr.Altitude <= 10000)
+                {
+                        if (dr.ImageQuality >= 1 && dr.ImageQuality <= 100)
+                        {
+                                return ValidationResult.Success();
+                        }
+                        else
+                            return ValidationResult.Failure("Invalid ImageQuality: must be between 1 - 100");
+                }
+                else
+                    return ValidationResult.Failure("Invalid Altitude: must be between 100 - 10000");
+            }
+        }
     }
 }
